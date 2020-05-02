3e8508331d48e2a281ee4b01c19a1b320c3d5ea5
@dan192596 <dperez@yetisworkshop.com>
2020-05-02 09:40:43 -0600

    Vistas

--- notas/views.py
+++ notas/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .forms import NotasForm
+from .models import Notas
 
 # Create your views here.
+def NotasCreateView(request):
+    if request.method == "POST":
+        form = NotasForm(request.POST)
+        if form.is_valid():
+            misnotas = form.save(commit=False)
+            misnotas.save()
+    form = NotasForm()    
+    return render(request, 'misnotas_list.html', {'form': form})
+
+def NotasListView(request):
+    queryset = Notas.objects.filter()
+    return render(request, 'misnotas_create.html', {'object_list':queryset})
